83097b9c60aa3cecce265e4c735f17fd9f351c65
Giordon Stark <kratsg@gmail.com>
2014-11-22 10:10:54 -0600

    Bump up version. Rewrote some of the logic for accessing elements of quora such as the login box, the login submit button, etcetera. Updated spec to reflect changes and test for these things.

--- lib/quora-webdriver/version.rb
+++ lib/quora-webdriver/version.rb
@@ -1,7 +1,7 @@
 module Watir
   class Browser
     class Quora
-      VERSION = "0.1.0"
+      VERSION = "0.1.2"
     end # Quora
   end # Browser
 end # Watir
--- spec/quora_spec.rb
+++ spec/quora_spec.rb
@@ -2,26 +2,36 @@ require 'spec_helper'
 
 browser = $browser
 
-quora_main_page = "https://www.quora.com/"
-
 RSpec.configure do |config|
   config.before(:all) { @browser = Watir::Browser.new(*$browser_params) }
   config.after(:all) { @browser.close unless @browser.nil? }
 end
 
-describe "a quora login page" do
+context "when quora.com is accessed" do
   before(:all) do
-    @browser.goto(quora_main_page)
+    @browser.goto @browser.quora.base_url
   end
 
-  describe "that we should have opened it" do
-    it "should not return an empty page" do
-      expect(@browser.text).not_to be_empty
-    end
-    it "should have login text" do
-      expect(@browser.text).to include("LOGIN\nRemember Me\nForgot Password?")
-    end
+  context "the text" do
+    subject { @browser.text }
+    it { is_expected.not_to be_empty }
+    it { is_expected.to include("Sign up to read Quora") }
+  end
 
+  context "add question div" do
+    subject { @browser.quora.get_add_question_div }
+    it { is_expected.not_to exist }
+  end
+  
+  context "login box" do
+    subject { @browser.quora.get_login_email_text }
+    it { is_expected.to exist }
+
+    subject { @browser.quora.get_login_password_text }
+    it { is_expected.to exist }
+
+    subject { @browser.quora.get_login_submit_button }
+    it { is_expected.to exist }
   end
 
 end
