b42f259de0ea62bb59e894cd0faaaf4cf85559bb
Giordon Stark <kratsg@gmail.com>
2014-11-22 07:10:05 -0600

    Added specs for initial login page with html file. THis doesn't check if it can find the login boxes yet though. Need to slightly refactor code to do this?

--- spec/quora_spec.rb
+++ spec/quora_spec.rb
@@ -1,5 +1,30 @@
-RSpec.describe "basic test" do
-  it "should pass" do
-    # pass
+require 'rspec'
+require 'quora-webdriver'
+
+browser = Watir::Browser.new :phantomjs
+quora_login_html = "file://#{File.expand_path("quora_login.html",File.dirname(__FILE__))}"
+
+RSpec.configure do |config|
+  config.before(:each) { @browser = browser }
+  config.after(:suite) { @browser.close unless @browser.nil? }
+end
+
+describe "a quora login page" do
+  before(:each) do
+    @browser.goto(quora_login_html)
   end
+
+  describe "that we should have opened it" do
+    it "should not return an empty page" do
+      expect(@browser.text).not_to be_empty
+    end
+    it "should have found the local html file" do
+      expect(@browser.text).not_to include("This webpage is not found")
+    end
+    it "should have loaded the html file" do
+      expect(@browser.text).to include("Sign up to read Quora")
+    end
+
+  end
+
 end
